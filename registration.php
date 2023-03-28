51b9ba583c76abe8a1605415b2460f04f6777abd
Jeanmady <62425251+Jeanmady@users.noreply.github.com>
2023-03-28 16:41:59 +0100

    added login bar

--- registration.php
+++ registration.php
@@ -6,6 +6,10 @@
         body {
             margin: 0;
             font: Verdana;
+            background-image: url('background-img.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: 100% 100%;
         }
 
         .topnav {
@@ -36,14 +40,16 @@
         .topnav a.split {
             float: right;
         }
-
-        body {
-            background-image: url('background-img.jpg');
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-            background-size: 100% 100%;
-        }
         
+        body, html {
+            height: 100%;
+        }
+
+        * {
+            box-sizing: border-box;
+        }
+
+        /* Add styles to the form container */
         .container {
             position: absolute;
             right: 0;
@@ -53,7 +59,8 @@
             background-color: white;
         }
 
-        input[type=text], input[type=password] {
+        /* Full-width input fields */
+            input[type=text], input[type=password] {
             width: 100%;
             padding: 15px;
             margin: 5px 0 22px 0;
@@ -65,6 +72,22 @@
             background-color: #ddd;
             outline: none;
         }
+
+        /* Set a style for the submit button */
+        .btn {
+            background-color: #89CFF0;
+            color: white;
+            padding: 16px 20px;
+            border: none;
+            cursor: pointer;
+            width: 100%;
+            opacity: 0.9;
+        }
+
+        .btn:hover {
+            opacity: 1;
+        }
+
     </style>
 </head>
 
@@ -75,10 +98,14 @@
         <a href="registration.php" class="active split">Leaderboard</a>
     </div>
     
-    <div class="form-box">
-        <form id="container">
-            <label for="uname">Username:</label><br>
-            <input type="text" id="uname" name="uname"><br>
+    <div class="username-login">
+        <form action="/action_page.phpPAGE FOR LOGIN BUTTTON BEING PRESSED" class="container"> 
+            <h1>Login</h1>
+
+            <label for="usrn"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="username" required>
+
+            <button type="submit" class="btn">Register</button>
         </form>
     </div>
 </body>
